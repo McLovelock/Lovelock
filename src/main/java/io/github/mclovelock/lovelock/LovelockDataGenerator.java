5517268ba56667a315c3e032cb5cef4c83c56001
59365069 <matija.brown@outlook.de>
2024-07-08 19:07:51 +0200

    Project initialisation

---
+++ src/main/java/io/github/mclovelock/lovelock/LovelockDataGenerator.java
@@ -0,0 +1,11 @@
+package io.github.mclovelock.lovelock;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class LovelockDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
